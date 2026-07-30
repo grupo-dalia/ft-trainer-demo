07d18fd0212706f4d098fb810b638fdf9c0a5e1e
Andres Cubero <andrescuberomoreno@gmail.com>
2026-07-30 14:32:01 +0200

    Actualizar clave publica de Supabase

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.FT_SUPABASE={
   url:'https://iehydmcjrbigfjzjguts.supabase.co',
-  publishableKey:'sb_publishable_ltaNA7nnVozoSCOcZIjg'
+  publishableKey:'sb_publishable_6qBDctEOxrhiN2tTiV9qIw_MM6SIcDO'
 };
